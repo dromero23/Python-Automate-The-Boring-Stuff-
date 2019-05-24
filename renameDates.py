c54f87e6b681a2e6160d50bd75b28bb6019b74aa
dromero23 <romero9286@gmail.com>
2019-05-24 18:35:46 -0500

    Add files via upload

--- renameDates.py
+++ renameDates.py
@@ -8,13 +8,14 @@ def main():
 	datePattern = re.compile(r"""^(.*?)
 	((0|1)?\d)-		# one or two digits for the month 
 	((0|1|2|3)?\d)-	# one or two digits for the day 
-	((19|20)\d\d)	#four digits for the year
+	((19|20)?\d\d)	#four digits for the year
 	(.*?)$			#all text after the date
 	""",re.VERBOSE)
 	
-	#TODO Call os.listdir() to find all the files in the working directory.
+	#Call os.listdir() to find all the files in the working directory.
+	#Loop over each filename using the regex to check whether it has a date.
 	for amerFilename in os.listdir('.'):
-		mo = datePattern.search(amerFileName)
+		mo = datePattern.search(amerFilename)
 		#skip files without a date
 		if mo == None: 
 			continue 
@@ -24,12 +25,14 @@ def main():
 		dayPart = mo.group(4)
 		yearPart = mo.group(6)
 		afterPart = mo.group(8)
-	#TODO Loop over each filename using the regex to check whether it has a date.
-	
-	#TODO If it has a date,rename the file with shutil.move()
-	
-
-
+		#Form the European-style filename.
+		euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
+		#Get the full, absolute file paths.
+		absWorkingDir = os.path.abspath('.')
+		amerFilename = os.path.join(absWorkingDir,amerFilename)
+		euroFilename = os.path.join(absWorkingDir,euroFilename)
+		#If it has a date,rename the file with shutil.move()
+		print('Renaming "%s" to "%s" ...'%(amerFilename,euroFilename))
 
 if __name__ == '__main__':
 	main()
