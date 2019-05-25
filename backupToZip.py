8f0e4db03b94ad5c2de788ac2b00fc2d6ab0571e
dromero23 <romero9286@gmail.com>
2019-05-24 19:45:12 -0500

    New file for backUpToZip.py

---
+++ backupToZip.py
@@ -0,0 +1,20 @@
+#!python3
+
+#backupToZip.py - copies an entire folder and its contents into
+#a ZIP file whose filename increments.
+
+import zipfile, os
+
+def main():
+
+
+
+def backupToZip(folder):
+	#backup the the entire contents of "folder" into a ZIP file.
+	
+	folder = os.path.abspath(folder) # make sure folder is absolute 
+	#Figure out the filename this colde should use based on 
+	#what files already exist 
+
+if __name__ = '__main__':
+	main()
