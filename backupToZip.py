c2ccc83de3bbdb5413c29d350169b9bdc1bf9fe7
dromero23 <romero9286@gmail.com>
2019-05-26 18:11:06 -0500

    version 1 of backupToZip.py
    From: http://automatetheboringstuff.com/chapter9/
    Instead of backing up 'delicious' the user inputs the file they want backedup. Also the zip file is the from the relative path instead of the absolute path

--- backupToZip.py
+++ backupToZip.py
@@ -3,18 +3,45 @@
 #backupToZip.py - copies an entire folder and its contents into
 #a ZIP file whose filename increments.
 
-import zipfile, os
+import zipfile, os,sys
 
 def main():
-
-
+	user_input = input('Enter the absolute folder path of your backup: ')
+	backupToZip(r'%s'%(user_input))
 
 def backupToZip(folder):
 	#backup the the entire contents of "folder" into a ZIP file.
-	
-	folder = os.path.abspath(folder) # make sure folder is absolute 
+	python_dir = os.getcwd()
+	os.chdir(folder)
+	folder = os.path.split(os.getcwd())[1] # make sure folder the base name
 	#Figure out the filename this colde should use based on 
 	#what files already exist 
-
-if __name__ = '__main__':
+	number = 1
+	while True: 
+		zipFilename = os.path.basename(folder)+ '_'+str(number)+'.zip'
+		if not os.path.exists(zipFilename):
+			break
+		number += 1 
+	# Create the ZIP file 
+	print ('Creating %s...'%(zipFilename))
+	backupZip = zipfile.ZipFile(zipFilename,'w')
+	
+	# Walk the entire folder tree and compress the files in each folder.
+	for foldername,subfolders, filenames in os.walk('.'):
+		if(foldername =='.'):
+			continue
+		print('Adding files in %s...'%(foldername))
+		#add the current folder to the ZIP file.
+		backupZip.write(foldername)
+		#add all the files in this folder to the zip file.
+		for filename in filenames:
+			newBase = os.path.basename(folder) + '_'
+			if filename.startswith(newBase) and filename.endswith('.zip'):
+				continue #don't backup the backup ZIP files
+			backupZip.write(os.path.join(foldername,filename))
+	backupZip.close()
+	os.chdir(python_dir) #change it back to the python directory 
+	print('Done.')
+	
+if __name__ == '__main__':
 	main()
