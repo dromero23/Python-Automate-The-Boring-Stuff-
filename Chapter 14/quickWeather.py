5b7abe45817cc91c85980ce9d493a43e6f770f9b
dromero23 <romero9286@gmail.com>
2019-07-20 20:13:40 -0500

    Check local weather
    API from: https://openweathermap.org

---
+++ Chapter 14/quickWeather.py
@@ -0,0 +1,32 @@
+#!python3 
+#quickWeather.py - downloads weather data from quickWeather.py
+
+import json, requests, sys
+from datetime import datetime
+
+#Compute location from command line arguments.
+
+
+def main():
+	if len(sys.argv)<2:
+		print('Usage: quickWeather.py [location]')
+		sys.exit()
+	location = ' '.join(sys.argv[1:])
+	#TODO: Download JSON data from OpenWeatherMap.org's API
+	url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s&cnt=17&appid=cd2bf382fd14abcd26fad1c24cd2aeff' % (location)
+	response = requests.get(url)
+	response.raise_for_status()
+	#Load JSON data into a Python variable.
+	weatherData = json.loads(response.text)
+	w = weatherData['list']
+	print('Current weater in %s: ' %(location))
+	print(w[0]['weather'][0]['main'], '-' ,w[0]['weather'][0]['description'])
+	print()
+	print ('Tomorrow around ' + datetime.now().strftime('%H:%M'))
+	print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+	print()
+	print('Day after tomorrow around:' + datetime.now().strftime('%H:%M'))
+	print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])	
+
+if __name__ =='__main__':
+	main()
